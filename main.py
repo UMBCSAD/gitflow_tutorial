5283d0d64af4d79f31c10620cc8fea8f51129db0
Henry <hbudris1@umbc.edu>
2022-04-01 18:41:31 -0400

    Initial commit of main

---
+++ main.py
@@ -0,0 +1,20 @@
+def isEven(num):
+    if num == 0: 
+        return True
+    elif num == 1:
+        return False;
+    elif num == 2:
+        return True
+    else:
+        print("Not implemented")
+        return False
+
+def main():
+    print("Are these numbers even?")
+
+    lst = [2,4,6,7,9]
+    for l in lst:
+        print(isEven(l))
+        
+
+main()
